8171353bea6d8d7761a69f3ed4007b5d2a920741
Wolfgang Born <post@gangflow.info>
2026-01-26 19:55:58 +0100

    format backup.sh
    Signed-off-by: Wolfgang Born <post@gangflow.info>

--- bash/backup.sh
+++ bash/backup.sh
@@ -5,7 +5,7 @@
 set -e
 
 show_help() {
-    cat << EOF
+  cat <<EOF
 Usage: $(basename "$0") [OPTIONS] SOURCE DESTINATION
 
 Backup files and directories to a specified location.
@@ -27,68 +27,68 @@ TIMESTAMP=false
 VERBOSE=false
 
 while [[ $# -gt 0 ]]; do
-    case $1 in
-        -h|--help)
-            show_help
-            exit 0
-            ;;
-        -c|--compress)
-            COMPRESS=true
-            shift
-            ;;
-        -t|--timestamp)
-            TIMESTAMP=true
-            shift
-            ;;
-        -v|--verbose)
-            VERBOSE=true
-            shift
-            ;;
-        -*)
-            echo "Unknown option: $1"
-            show_help
-            exit 1
-            ;;
-        *)
-            break
-            ;;
-    esac
+  case $1 in
+    -h | --help)
+      show_help
+      exit 0
+      ;;
+    -c | --compress)
+      COMPRESS=true
+      shift
+      ;;
+    -t | --timestamp)
+      TIMESTAMP=true
+      shift
+      ;;
+    -v | --verbose)
+      VERBOSE=true
+      shift
+      ;;
+    -*)
+      echo "Unknown option: $1"
+      show_help
+      exit 1
+      ;;
+    *)
+      break
+      ;;
+  esac
 done
 
 if [ $# -lt 2 ]; then
-    echo "Error: SOURCE and DESTINATION are required"
-    show_help
-    exit 1
+  echo "Error: SOURCE and DESTINATION are required"
+  show_help
+  exit 1
 fi
 
 SOURCE="$1"
 DESTINATION="$2"
 
 if [ ! -e "$SOURCE" ]; then
-    echo "Error: Source '$SOURCE' does not exist"
-    exit 1
+  echo "Error: Source '$SOURCE' does not exist"
+  exit 1
 fi
 
 if [ ! -d "$DESTINATION" ]; then
-    echo "Error: Destination directory '$DESTINATION' does not exist"
-    exit 1
+  echo "Error: Destination directory '$DESTINATION' does not exist"
+  exit 1
 fi
 
 BACKUP_NAME=$(basename "$SOURCE")
 
 if [ "$TIMESTAMP" = true ]; then
-    TIMESTAMP_STR=$(date +%Y%m%d_%H%M%S)
-    BACKUP_NAME="${BACKUP_NAME}_${TIMESTAMP_STR}"
+  TIMESTAMP_STR=$(date +%Y%m%d_%H%M%S)
+  BACKUP_NAME="${BACKUP_NAME}_${TIMESTAMP_STR}"
 fi
 
 if [ "$COMPRESS" = true ]; then
-    BACKUP_FILE="${DESTINATION}/${BACKUP_NAME}.tar.gz"
-    [ "$VERBOSE" = true ] && echo "Creating compressed backup: $BACKUP_FILE"
-    tar -czf "$BACKUP_FILE" -C "$(dirname "$SOURCE")" "$(basename "$SOURCE")"
-    echo "Backup created: $BACKUP_FILE"
+  BACKUP_FILE="${DESTINATION}/${BACKUP_NAME}.tar.gz"
+  [ "$VERBOSE" = true ] && echo "Creating compressed backup: $BACKUP_FILE"
+  tar -czf "$BACKUP_FILE" -C "$(dirname "$SOURCE")" "$(basename "$SOURCE")"
+  echo "Backup created: $BACKUP_FILE"
 else
-    BACKUP_PATH="${DESTINATION}/${BACKUP_NAME}"
-    [ "$VERBOSE" = true ] && echo "Copying to: $BACKUP_PATH"
-    cp -r "$SOURCE" "$BACKUP_PATH"
-    echo "Backup created: $BACKUP_PATH"
+  BACKUP_PATH="${DESTINATION}/${BACKUP_NAME}"
+  [ "$VERBOSE" = true ] && echo "Copying to: $BACKUP_PATH"
+  cp -r "$SOURCE" "$BACKUP_PATH"
+  echo "Backup created: $BACKUP_PATH"
 fi
